4ac3151ef6c2f02b4c65b3ef3969ace112169f7c
Teemu Kakko <sormiteippi@windowslive.com>
2024-11-23 22:52:49 +0200

    Start editing menu buttons

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -10,7 +10,7 @@ const MenuButton: FC<Props> = ({ tag, isPressed, handleClick }) => {
   return (
     <button
       aria-pressed={isPressed}
-      className='m-1 rounded-3xl bg-neutral-100 px-5 py-2.5 font-montserrat text-xs font-light text-green-900 hover:bg-neutral-200 aria-pressed:bg-green-900 aria-pressed:text-white aria-pressed:hover:bg-green-900 sm:text-sm md:text-base'
+      className='m-1 rounded-3xl bg-neutral-200 px-3 py-2 font-montserrat text-xs font-neutral hover:bg-neutral-200 aria-pressed:bg-neutral-600 aria-pressed:text-white aria-pressed:hover:bg-green-900 sm:text-sm md:text-base'
       onClick={() => handleClick(tag.value)}
     >
       {tag.label}
